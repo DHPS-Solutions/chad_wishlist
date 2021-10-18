8eaafb38364294d8a4808285524c434ec88d5929
CJGutz <cjgutzkow@gmail.com>
2021-10-18 19:51:04 +0200

    feat(wishlists): added item inline for wishlists

--- backendChad/wishlists/models.py
+++ backendChad/wishlists/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from items.models import Item
+
 # Create your models here.
 
 class Wishlist(models.Model):
@@ -10,4 +12,10 @@ class Wishlist(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
+    def __str__(self):
+        return self.title
 
+class ItemsOnWishlist(models.Model):
+
+    item = models.ForeignKey(Item, on_delete=models.RESTRICT, default=None)
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.RESTRICT, default=None)
