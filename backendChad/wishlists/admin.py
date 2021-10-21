891ede17aff25cb803d1a3325bf749fa116cf71d
CJGutz <cjgutzkow@gmail.com>
2021-10-21 17:31:21 +0200

    fix(accounts): fixed migerations and database

--- backendChad/wishlists/admin.py
+++ backendChad/wishlists/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
 
-from wishlists.models import Wishlist
+from wishlists.models import ItemsOnWishlist, Wishlist
 
+
+#admin.site.register(ItemsOnWishlist)
+
+class ItemsOnWishlistInline(admin.TabularInline):
+    model = ItemsOnWishlist
 
 @admin.register(Wishlist)
 class WishlistAdmin(ModelAdmin):
     base_model = Wishlist
+    inlines = [
+        ItemsOnWishlistInline,
+    ]
+
+
